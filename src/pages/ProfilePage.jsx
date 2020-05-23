3dcc521de013dad8aed10b66022411f6c62ec7d9
avtamanov <avtamanov@edu.hse.ru>
2020-05-23 02:55:04 +0200

    image commit

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Image from '../components/Image';
+import {Box} from 'reflexbox';
 
 export default function ProfilePage() {
   return (
     <>
       <div>
-
+          <Box
+              as={Image}
+              src='https://im0-tub-ru.yandex.net/i?id=48ac536c899eff440e5f41fe226bd070&n=13&exp=1'
+              css={`
+              width: 280px;
+              height: 200px;
+              `}
+          />
       </div>
     </>
   );
